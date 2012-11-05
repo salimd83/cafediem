ceeda101f90620cfd25208e7f4108e56e3efe2b4
Salim Dirani <email@example.com>
2012-11-05 17:56:09 +0200

    retore quiz.php

--- quiz.php
+++ quiz.php
@@ -1,5 +1,15 @@
 <?php session_start() ?>
 
+<?php 
+require_once('models/questions.php');
+require_once('models/question.php');
+require_once('models/choice.php');
+
+require_once('includes/create_conn.php');
+
+$questions = Questions::retrieveAll($conn);
+?>
+
 <!DOCTYPE html>
 
 <html>
@@ -24,6 +34,7 @@
 			<div id="question">
 				<div class="batch">
 					<p class="error"></p>
+
 					<?php
 						$i=0;
 						foreach($questions as $question):
@@ -32,204 +43,54 @@
 							$type = $question->getType();
 							$id = $question->getId();
 					?>
-					<div>
-						<!-- <img src="images/questions/question-1.gif" 
-							 alt="Where Do You Live?"
-							 width="220"
-							 height="129" /> -->
-						<!-- <dl>
-							<dt><?php printf('%02d',$i); ?></dt>
-							<dd><?php echo $sQuestion; ?></dd>
-						</dl>
-						<ul>
-							<?php if($type == 'open'): ?>
-								<textarea placeholder="Answer goes here..." name="<?php echo $id; ?>"></textarea>
-							<?php else: ?>
-								<select 
-										name="<?php echo $id; ?>" 
-										id="<?php echo $id; ?>"
-										class="choices" 
-										<?php if($type == 'multichoice')  
-													echo "multiple size='2' style='height:86px'"; ?>
-								>
-									<?php $choices = $question->getChoices($conn); ?>
-									<?php if($type != 'multichoice'): ?>
-										<option style="color:#ccc;" selected="selected" value="0">
-											Choose answer
-										</option>
-									<?php endif; ?>
 
-									<?php foreach($choices as $choice): ?>
-										<option value="<?php echo $choice->getId(); ?>">
-											<?php echo ucwords(html_entity_decode($choice->getAnswer())); ?>
-										</option>
-										<li>
-											<a href="#" name="<?php echo $choice->getId(); ?>">
-												<?php echo ucwords(html_entity_decode($choice->getAnswer())); ?>
-											</a>
-										</li>
-									<?php endforeach; ?>
+							<div>
+								<dl>
+									<dt><?php printf('%02d',$i); ?></dt>
+									<dd>
+										<h2><?php echo $sQuestion; ?></h2>
+										<?php if($type == 'open'): ?>
+											<textarea placeholder="Answer goes here..." name="<?php echo $id; ?>"></textarea>
+										<?php else: ?>
+											<select 
+													name="<?php echo $id; ?>" 
+													id="<?php echo $id; ?>"
+													class="choices" 
+													<?php if($type == 'multichoice')  
+																echo "multiple size='2' style='height:86px'"; ?>
+											>
+											<?php $choices = $question->getChoices($conn); ?>
+											<?php if($type != 'multichoice'): ?>
+												<option style="color:#ccc;" selected="selected" value="0">
+													Choose answer
+												</option>
+											<?php endif; ?>
+											<?php foreach($choices as $choice): ?>
+												<option value="<?php echo $choice->getId(); ?>">
+													<?php echo ucwords(html_entity_decode($choice->getAnswer())); ?>
+												</option>
+											<?php endforeach; ?>
+											<?php if($i <= 2): ?>
+												<option class="other" value="other">Other</option>
+											<?php endif; ?>
+											</select>
+										<?php endif; ?>
+									</dd>
+								</dl>
+							</div>
 
-									<?php if($i <= 2): ?>
-										<li class="other">
-											<a href="#" name="<?php echo $choice->getId(); ?>">
-												<?php echo ucwords(html_entity_decode($choice->getAnswer())); ?>
-											</a>
-										</li>
-									<?php endif; ?>
-								</select>
+							<?php if($i == 10): ?>
+
+								</div> <!-- END of (first) .batch -->
+
+								<div class="batch">
+									<p class="error"></p>
+
 							<?php endif; ?>
-						</ul> -->
 
-						<dl>
-							<dt><?php printf('%02d',$i); ?></dt>
-							<dd><?php echo $sQuestion; ?></dd>
-						</dl>
-						<ul>
-							<?php if($type == 'open'): ?>
-								<textarea placeholder="Answer goes here..." name="<?php echo $id; ?>"></textarea>
-							<?php else: ?>
-								<select 
-										name="<?php echo $id; ?>" 
-										id="<?php echo $id; ?>"
-										class="choices" 
-										<?php if($type == 'multichoice')  
-													echo "multiple size='2' style='height:86px'"; ?>
-								>
-									<?php $choices = $question->getChoices($conn); ?>
-									<?php if($type != 'multichoice'): ?>
-										<option style="color:#ccc;" selected="selected" value="0">
-											Choose answer
-										</option>
-									<?php endif; ?>
+					<?php endforeach; ?>
 
-									<?php foreach($choices as $choice): ?>
-										<option value="<?php echo $choice->getId(); ?>">
-											<?php echo ucwords(html_entity_decode($choice->getAnswer())); ?>
-										</option>
-										<li>
-											<a href="#" name="<?php echo $choice->getId(); ?>">
-												<?php echo ucwords(html_entity_decode($choice->getAnswer())); ?>
-											</a>
-										</li>
-									<?php endforeach; ?>
-									
-									<?php if($i <= 2): ?>
-										<li class="other">
-											<a href="#" name="<?php echo $choice->getId(); ?>">
-												<?php echo ucwords(html_entity_decode($choice->getAnswer())); ?>
-											</a>
-										</li>
-									<?php endif; ?>
-								</select>
-							<?php endif; ?>
-						</ul>
-					</div>
-				</div> <!-- END of (first) .batch -->
-
-				<div class="batch">
-					<p class="error"></p>
-					<div>
-						<dl>
-							<dt>13</dt>
-							<dd>what’s your favorite sandwich?</dd>
-						</dl>
-						<ul>
-							<li><a href="#">Chicken Avocado</a></li>
-							<li><a href="#">Crab Cocktail</a></li>
-							<li><a href="#">Ham & Cheese</a></li>
-							<li><a href="#">Halloumi</a></li>
-						</ul>
-					</div>
-					<div>
-						<dl>
-							<dt>14</dt>
-							<dd>what’s your favorite salad?</dd>
-						</dl>
-						<textarea id="favorite-salad" placeholder="Answer goes here..."></textarea>
-					</div>
-					<div>
-						<dl>
-							<dt>15</dt>
-							<dd>What’s the best bean used for coffee?</dd>
-						</dl>
-						<ul>
-							<li><a href="#">Arabica</a></li>
-							<li><a href="#">Robusta</a></li>
-							<li><a href="#">Botanica</a></li>
-						</ul>
-					</div>
-					<div>
-						<dl>
-							<dt>16</dt>
-							<dd>Is cappuccino made from milk or cream?</dd>
-						</dl>
-						<ul class="two-rows">
-							<li><a href="#">Milk</a></li>
-							<li><a href="#">Cream</a></li>
-						</ul>
-					</div>
-					<div>
-						<dl>
-							<dt>17</dt>
-							<dd>when was the sandwich invented?</dd>
-						</dl>
-						<ul>
-							<li><a href="#">17<sup>th</sup> Century</a></li>
-							<li><a href="#">18<sup>th</sup> Century</a></li>
-							<li><a href="#">19<sup>th</sup> Century</a></li>
-						</ul>
-					</div>
-					<div>
-						<dl>
-							<dt>18</dt>
-							<dd>what item do you rarely find in a salad bar?</dd>
-						</dl>
-						<textarea id="favorite-salad" 
-								  placeholder="Answer goes here...">
-						</textarea>
-					</div>
-					<div>
-						<dl>
-							<dt>19</dt>
-							<dd>what’s the difference between organic 
-								and natural?</dd>
-						</dl>
-						<textarea id="favorite-salad" 
-								  placeholder="Answer goes here...">
-						</textarea>
-					</div>
-					<div>
-						<dl>
-							<dt>20</dt>
-							<dd>What wine goes best with cheese?</dd>
-						</dl>
-						<ul>
-							<li><a href="#">Red Wine</a></li>
-							<li><a href="#">White Wine</a></li>
-							<li><a href="#">Rosé Wine</a></li>
-							<li><a href="#">Sparkling Wine</a></li>
-						</ul>
-					</div>
-					<div>
-						<dl>
-							<dt>21</dt>
-							<dd>what’s the difference between creme caramel 
-								and creme brulee?</dd>
-						</dl>
-						<textarea id="favorite-salad" 
-								  placeholder="Answer goes here...">
-						</textarea>
-					</div>
-					<div>
-						<dl>
-							<dt>22</dt>
-							<dd>What does “Carpe Diem” mean to you?</dd>
-						</dl>
-						<textarea id="favorite-salad" 
-								  placeholder="Answer goes here...">
-						</textarea>
-					</div>
+				
 
 					<div class="socials">
 						<div class="m-default">
@@ -261,7 +122,7 @@
 						</div>
 					</div> <!-- END .socials -->
 
-					<input type="button" id="submit" value="Send" />
+					<input type="button" id="submit" value="Submit Answers" />
 				</div> <!-- END of (second and last) .batch -->
 				<div class="clear"></div>
 			</div><!-- END #questions -->
